888add833b7d2b2b2568c3906b6b915d8f7bef8d
Dominik Grygierzec <dominikgrygierzec@gmx.de>
2017-03-14 15:55:10 +0100

    Add custom css

--- app/app.jsx
+++ app/app.jsx
@@ -6,9 +6,13 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+//Load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css');
-
 $(document).foundation();
+
+//App css
+require('style!css!applicationStyles');
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
